640ff2ffb46be9aafd9fffb5a91fa1e335a27417
chenyomi <408550179@qq.com>
2026-02-05 00:53:03 +0800

    fix: 更新

--- src/components/landing/StartBuilding/StartBuilding.vue
+++ src/components/landing/StartBuilding/StartBuilding.vue
@@ -13,8 +13,29 @@
           style="position: absolute; left: 0; top: 0"
         />
         <h4 class="start-building-title">Case</h4>
-        <p class="start-building-subtitle">功能介绍</p>
+        <p class="start-building-subtitle mt-4">基础付费版功能介绍</p>
         <video src="@/assets/video/video.mov" controls class="w-200" style="border-radius: 20px"></video>
+        <p class="start-building-subtitle mt-10">高级进阶版（暂不开放）</p>
+        <div class="flex w-200 gap-20 items-end">
+          <div class="flex-1" style="background: #ffffff; border-radius: 20px">
+            <img style="border-radius: 20px" src="@/assets/shu.png" alt="" />
+          </div>
+          <div class="flex-1" style="background: #ffffff; border-radius: 20px">
+            <video
+              controls
+              src="@/assets/video/cir_.mp4"
+              :autoplay="true"
+              :loop="true"
+              style="border-radius: 20px"
+            ></video>
+          </div>
+        </div>
+        <div class="flex w-200 gap-20">
+          <p class="flex-1">竖排文字</p>
+          <p class="flex-1">弧形文字</p>
+        </div>
+        <p class="start-building-subtitle mt-10">Playground</p>
+        <p class="start-building-subtitle">基础付费版功能在线体验入口（不包含进阶高级版）</p>
         <router-link custom to="" class="start-building-button">
           <a href="https://stackblitz.com/~/github.com/chenyomi/leafer-htmltext-edit-view" target="_blank">
             Just Trying Now
