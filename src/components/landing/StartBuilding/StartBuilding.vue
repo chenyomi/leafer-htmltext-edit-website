59e44b508bba8f57d978dea1281301813d6052d5
chenyomi <408550179@qq.com>
2026-02-04 17:18:16 +0800

    fix: 更新

--- src/components/landing/StartBuilding/StartBuilding.vue
+++ src/components/landing/StartBuilding/StartBuilding.vue
@@ -16,10 +16,7 @@
         <p class="start-building-subtitle">功能介绍</p>
         <video src="@/assets/video/video.mov" controls class="w-200" style="border-radius: 20px"></video>
         <router-link custom to="" class="start-building-button">
-          <a
-            href="https://stackblitz.com/~/github.com/chenyomi/leafer-htmltext-edit-view?file=src/components/HelloWorld.vue"
-            target="_blank"
-          >
+          <a href="https://stackblitz.com/~/github.com/chenyomi/leafer-htmltext-edit-view" target="_blank">
             Just Trying Now
           </a>
         </router-link>
