a2bff910588b1d195d9f3332ef4acdecd4e9a22c
GitHub Action <action@github.com>
2026-06-08 06:13:24 +0000

    docs: sync external plugin docs [skip ci]

--- src/pages/DocsPage.vue
+++ src/pages/DocsPage.vue
@@ -1211,7 +1211,10 @@ const changelog = [
     version: '2.5.3',
     date: '2026-06',
     tag: 'patch',
-    items: ['更新版本号至 2.5.3', '在 dateEdit 方法中使用 resolveHTMLTextLeaf 处理回调参数']
+    items: [
+      '更新版本号至 2.5.3',
+      '在 dateEdit 方法中使用 resolveHTMLTextLeaf 处理回调参数'
+    ]
   },
   {
     version: '2.5.2',
